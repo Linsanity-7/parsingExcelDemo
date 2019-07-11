7768a1ba8a06830a26896ad9e35e1394d45a55fc
Linsanity-7 <l409506800@163.com>
2019-07-11 23:17:26 +0800

    首次提交

--- src/main/java/com/example/excel/demo/demo/TestDemo.java
+++ src/main/java/com/example/excel/demo/demo/TestDemo.java
@@ -1,5 +1,7 @@
 package com.example.excel.demo.demo;
 
+import com.example.excel.demo.thread.DealWithProductInfoExcelThread;
+
 /**
  * @apiNote 测试类
  * @author linshanghua
@@ -7,6 +9,7 @@ package com.example.excel.demo.demo;
 public class TestDemo {
 
     public static void main(String[] args) {
-
+        DealWithProductInfoExcelThread thread = new DealWithProductInfoExcelThread("");
+        thread.run();
     }
 }
